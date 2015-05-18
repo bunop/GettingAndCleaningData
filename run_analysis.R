8d38f0ecad56a093a6e401207f957ee82c455b1d
Paolo Cozzi <paolo.cozzi@tecnoparco.org>
2015-05-18 14:49:19 +0200

    Reading test set

--- run_analysis.R
+++ run_analysis.R
@@ -13,7 +13,7 @@ data_file <- file.path(tempdir(), "uci_har_dataset.zip")
 
 # Download data in a temporary directory if needed
 if (! file.exists(data_file)) {
-    cat(data_file, "doesn't exists. Downloading it...")
+    cat(data_file, "doesn't exists. Downloading it into", tempdir(), "...")
     download.file(data_url, data_file, method = "curl")
     cat("Done!")
 }
@@ -23,8 +23,8 @@ file_list <- unzip(data_file, list = T)
 
 # Try to determine if data_file is already unpacked
 # (check if the first element in file list exists in temporay directory)
-if (! file.exists(file_list[1,1])) {
-    cat("Unzipping", data_file, "into", getwd(), "...")
+if (! file.exists(file.path(tempdir(), file_list[1,1]))) {
+    cat("Unzipping", data_file, "into", tempdir(), "...")
     unzip(data_file, exdir=tempdir())
 }
 
@@ -32,6 +32,11 @@ if (! file.exists(file_list[1,1])) {
 idx <- grep("X_train", file_list$Name)
 train_file <- file_list$Name[idx]
 train_path <- file.path(tempdir(), train_file)
+
+# get the relative X_test path in archive
+idx <- grep("X_test", file_list$Name)
+test_file <- file_list$Name[idx]
+test_path <- file.path(tempdir(), test_file)
 
 # get features list (data headers)
 idx <- grep("features.txt", file_list$Name)
@@ -41,5 +46,5 @@ features_path <- file.path(tempdir(), features_file)
 # read the training set
 training_set <- read_handy(train_path, features_path)
 
-
-
+# read the test set
+test_set <- read_handy(test_path, features_path)
