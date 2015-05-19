ee29c758c17f88b1db93f6e516647831778561a1
Paolo Cozzi <paolo.cozzi@tecnoparco.org>
2015-05-19 15:16:18 +0200

    Moved code from run_analysis to helper
    I moved some functions from run_analysis.R to helper.R

--- helper.R
+++ helper.R
@@ -1,6 +1,48 @@
 
 # An helper file to do some stuff
 
+# Donwload uci har dataset if needed
+downloadDataset <- function(directory=tempdir()) {
+    # This is the data url of an archive zip
+    data_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+    # This is the name of the downloaded file
+    data_file <- file.path(directory, "uci_har_dataset.zip")
+
+    # Download data in a temporary directory if needed
+    if (! file.exists(data_file)) {
+        cat(data_file, "doesn't exists. Downloading it into", tempdir(), "...")
+        download.file(data_url, data_file, method = "curl")
+        cat("Done!")
+    }
+
+    #return data_file path
+    return(data_file)
+}
+
+# Unpack data if needed
+unpackDataset <- function(data_file, directory=tempdir()) {
+    file_list <- unzip(data_file, list = T)
+
+    # Try to determine if data_file is already unpacked
+    # (check if the first element in file list exists in temporay directory)
+    if (! file.exists(file.path(directory, file_list[1,1]))) {
+        cat("Unzipping", data_file, "into", directory, "...")
+        unzip(data_file, exdir=directory)
+    }
+
+    #return the file lista dataframe
+    return(file_list)
+}
+
+# A function to get file path by givin' file name
+getFilePath <- function(name, file_list, directory=tempdir()) {
+    idx <- grep(name, file_list$Name)
+    file_name <- file_list$Name[idx]
+    file_path <- file.path(directory, file_name)
+}
+
+
 #define a function to get a label from a number. Use activity label to get labels
 Activity <- function(x = data.frame()) {
     init <- function(activity_file) {
--- run_analysis.R
+++ run_analysis.R
@@ -5,61 +5,39 @@ source('~/Dropbox/Coursera/GettingAndCleaningData/helper.R')
 # Record this working directory
 working_dir <- getwd()
 
-# This is the data url of an archive zip
-data_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+# get UCI HAR dataset (from helper.R)
+data_file <- downloadDataset()
 
-# This is the name of the downloaded file
-data_file <- file.path(tempdir(), "uci_har_dataset.zip")
+# getting the file list (from helper.R)
+file_list <- unpackDataset(data_file)
 
-# Download data in a temporary directory if needed
-if (! file.exists(data_file)) {
-    cat(data_file, "doesn't exists. Downloading it into", tempdir(), "...")
-    download.file(data_url, data_file, method = "curl")
-    cat("Done!")
-}
-
-# getting the file list
-file_list <- unzip(data_file, list = T)
-
-# Try to determine if data_file is already unpacked
-# (check if the first element in file list exists in temporay directory)
-if (! file.exists(file.path(tempdir(), file_list[1,1]))) {
-    cat("Unzipping", data_file, "into", tempdir(), "...")
-    unzip(data_file, exdir=tempdir())
-}
-
-# get the relative X_train path in archive
-idx <- grep("X_train", file_list$Name)
-train_file <- file_list$Name[idx]
-train_path <- file.path(tempdir(), train_file)
+# get the relative X_train path in archive. getFilePath is defined in helper.r
+train_path <- getFilePath("X_train", file_list)
 
 # get the relative y_train path in archive
-idx <- grep("/y_train.txt", file_list$Name)
-train_file_labels <- file_list$Name[idx]
-train_path_labels <- file.path(tempdir(), train_file_labels)
+train_path_labels <- getFilePath("/y_train.txt", file_list)
 
 # get the relative X_test path in archive
-idx <- grep("X_test", file_list$Name)
-test_file <- file_list$Name[idx]
-test_path <- file.path(tempdir(), test_file)
+test_path <- getFilePath("X_test", file_list)
 
 # get the relative y_test path in archive
-idx <- grep("/y_test.txt", file_list$Name)
-test_file_labels <- file_list$Name[idx]
-test_path_labels <- file.path(tempdir(), test_file_labels)
+test_path_labels <- getFilePath("/y_test.txt", file_list)
 
 # get features list (data headers)
-idx <- grep("features.txt", file_list$Name)
-features_file <- file_list$Name[idx]
-features_path <- file.path(tempdir(), features_file)
+features_path <- getFilePath("features.txt", file_list)
 
 # get activity labels
-idx <- grep("activity_labels.txt", file_list$Name)
-activity_labels <- file_list$Name[idx]
-activity_labels_path <- file.path(tempdir(), activity_labels)
+activity_labels_path <- getFilePath("activity_labels.txt", file_list)
 
-# read the training set
+# read the training set (function defined in helper.R)
 training_set <- read_handy(train_path, features_path, train_path_labels, activity_labels_path)
 
 # read the test set
 test_set <- read_handy(test_path, features_path, test_path_labels, activity_labels_path)
+
+# Put the two test together (since names are the same)
+merged_set <- rbind(training_set, test_set)
+
+# remove un used data frames
+rm(training_set)
+rm(test_set)
